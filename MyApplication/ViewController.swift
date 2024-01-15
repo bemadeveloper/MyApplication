61dd9df84e6e7715a1127dd2abb0ed6fa7b063b5
bemadeveloper <akbermet1990@icloud.com>
2024-01-15 16:34:14 +0600

    Clean comments

--- MyApplication/ViewController.swift
+++ MyApplication/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  MyApplication
-//
-//  Created by Bema on 15/1/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
